b452cba5b340cf0122cb8152ae4b4cfca0d28ff4
chankyo123 <ckkim10@gmail.com>
2024-10-30 15:42:40 +0000

    recent code

--- src/network/model_factory.py
+++ src/network/model_factory.py
@@ -1,6 +1,22 @@
 # from network.eq_resnet_1res_bd import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
-from network.eq_resnet_2res_bd import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+# from network.eq_resnet_2res_bd import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
 # from network.eq_resnet_3res_bd import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+# from network.eq_resnet_4res_bd import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+
+#### regress 2 output (mean, covariance)
+# from network.eq_resnet_2res_2regress import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+# from network.eq_resnet_4res_2regress import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+from network.eq_resnet_4res_2regress_more_param import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+# from network.vn_resnet_4res_2regress import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
+# from network.ln_2regress import SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress import SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress_less_param import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress_nochannelmix import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress_nochannelmix_slope_1_largechannel_onechannelmix import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
+from network.ln_conv_2regress_nochannelmix_slope_1 import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress_nochannelmix_slope_1_onechannelmix import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
+# from network.ln_conv_2regress_nochannelmix_slope_1_onechannelmix_param16_fc2 import LN_BasicBlock1D, SO3EquivariantReluBracketLayers    #6D or 9D
 
 # from network.eq_resnet_2res import VN_BasicBlock1D, VN_ResNet1D
 # from network.eq_resnet_1res import VN_BasicBlock1D, VN_ResNet1D
@@ -16,7 +32,8 @@ from network.eq_resnet_2res_bd import VN_BasicBlock1D, VN_ResNet1D    #6D or 9D
 
 # from network.model_vn_resnet_2 import VN_BasicBlock1D, VN_ResNet1D
 
-from network.model_resnet import BasicBlock1D, ResNet1D
+# from network.model_resnet import BasicBlock1D, ResNet1D
+from network.model_resnet_2res_nodrop import BasicBlock1D, ResNet1D
 
 
 # from network.model_resnet_2res_nodrop import BasicBlock1D, ResNet1D
@@ -41,6 +58,13 @@ def get_model(arch, net_config, input_dim=6, output_dim=3):
         network = ResNetSeq1D(
             BasicBlock1D, input_dim, output_dim, [2, 2, 2, 2], net_config["in_dim"]
         )
+    elif arch == "ln_resnet":
+            network = SO3EquivariantReluBracketLayers(
+                LN_BasicBlock1D,
+                input_dim, output_dim, 
+                [2, 2, 2, 2],
+                net_config["in_dim"]
+            )
     elif arch == "tcn":
         network = TlioTcn(
             input_dim,
